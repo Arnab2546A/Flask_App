d901dd5f084268eb6b27400b6639d475205a8953
Arnab2546A <duaaranb@gmail.com>
2026-02-06 03:38:48 +0530

    modifying flask WTF form.py

--- flask-WTF/form.py
+++ flask-WTF/form.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Email, Length
 
 class RegistrationForm(FlaskForm):
     name=StringField("Full Name", validators=[DataRequired()])
-    email=StringField("Email", validators=[DataRequired(), Email()])
+    email=StringField("Email", validators=[DataRequired(), Email(message="yaha imaandari chalat hai babu..na toh ghare jaake sutti babu..")])
     password=PasswordField("Password", validators=[DataRequired(), Length(min=6)])
     submit=SubmitField("Register")

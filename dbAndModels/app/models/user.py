a484c80e22fb7d9b1a50cc9acd1d347ce8c56cac
Arnab2546A <duaaranb@gmail.com>
2026-02-09 15:05:09 +0530

    using SQLAlchemy

---
+++ dbAndModels/app/models/user.py
@@ -0,0 +1,13 @@
+
+from .. import db
+
+# defines the table
+# class -> table
+# variable -> column
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name=db.Column(db.String(100),nullable=False)
+	email=db.Column(db.String(200),unique=True)
+	posts=db.relationship('Post',backref='author',lazy=True)
+	
